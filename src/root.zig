61b488c1bd4e2861a68a23cb7bf59e79be725110
PierreLgol <plgol.perso@gmail.com>
2025-03-15 11:31:12 +0100

    removing stuff

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 
-const BinaryTree = @import("BinaryTree.zig");
 const Optional = @import("Optional.zig");
 const Result = @import("Result.zig");
-const SlotMap = @import("SlotMap.zig");
-const Stack = @import("Stack.zig");
 
 comptime {
-    std.testing.refAllDeclsRecursive(BinaryTree);
-    std.testing.refAllDeclsRecursive(Stack);
     std.testing.refAllDeclsRecursive(Result);
     std.testing.refAllDeclsRecursive(Optional);
 }
